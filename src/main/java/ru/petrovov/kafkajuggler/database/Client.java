d4db1582c780ce8cfd85133f5ae0293c27d0aff5
Oleg Petrov <olegp117@gmail.com>
2023-02-28 22:24:09 +0300

    KJ-8 - fixed ToString problem for Client entity. Added docker compose for Kafka container.

--- src/main/java/ru/petrovov/kafkajuggler/database/Client.java
+++ src/main/java/ru/petrovov/kafkajuggler/database/Client.java
@@ -1,17 +1,21 @@
 package ru.petrovov.kafkajuggler.database;
 
 import lombok.Data;
+import lombok.ToString;
 
 import javax.persistence.*;
 import java.util.List;
 
 @Entity
 @Data
+@ToString(onlyExplicitlyIncluded = true)
 public class Client {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @ToString.Include
     private String firstName;
+    @ToString.Include
     private String secondName;
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "client")
     private List<Paper> papers;
